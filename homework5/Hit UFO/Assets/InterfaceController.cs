d5377b4934456f51d9433a7fd371f4727c0eab9a
zengty <1258632312@qq.com>
2020-10-28 22:25:49 +0800

    hw5

---
+++ homework5/Hit UFO/Assets/InterfaceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISceneController{
+    void LoadResources();
+}
+
+public interface IUserAction{
+    void Hit(Vector3 pos);
+    void Restart();
+    int GetScore();
+    void GameOver();
+    bool isCounting();
+    int getEmitTime();
+    void setting(float speed, GameObject explosion);
+}
